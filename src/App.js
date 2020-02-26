5c063b6196d999e71276a2a057ffed6054c3eda6
TommiS <tommi.supi@gmail.com>
2020-02-26 10:17:54 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './App.css';
 import Sidebar from "./Sidebar";
+import Navbartesti from "./Navbartesti";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
 
-        <Sidebar/>
+    return (
+        <div className="App">
+            <header className="App-header">
 
-      </header>
-    </div>
-  );
+                <Sidebar/>
+                <Navbartesti/>
+
+            </header>
+        </div>
+    );
 }
 
 export default App;
